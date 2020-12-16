4eeee29131bcba8b4ef2d8649eb4050d9fc12fd2
L-TANK <214927409@qq.com>
2020-12-16 09:42:28 +0800

    修改了12/15的代码

--- LeetCode738.java
+++ LeetCode738.java
@@ -1,19 +1,17 @@
 package com.LY.LeetCode;
 
 public class LeetCode738 {
-    //单调递增的数字
+    /**
+     * 单调递增的数字
+     * 思路：
+     *  从右向左扫描数字，若发现当前数字比其左边一位（较高位）小，
+     *  则把其左边一位数字减1，并将该位及其右边的所有位改成9
+     */
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.monotoneIncreasingDigits(332));//229
+        System.out.println(monotoneIncreasingDigits(332));//229
     }
-}
-/**
- * 思路：
- *  从右向左扫描数字，若发现当前数字比其左边一位（较高位）小，
- *  则把其左边一位数字减1，并将该位及其右边的所有位改成9
- */
-class Solution {
-    public int monotoneIncreasingDigits(int N) {
+
+    public static int monotoneIncreasingDigits(int N) {
         String s = String.valueOf(N);
         char[] chars = s.toCharArray();
         int length = s.length();
@@ -32,5 +30,7 @@ class Solution {
         return Integer.parseInt(new String(chars));
     }
 }
+
+
 
 
