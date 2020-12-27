1dc6b95d7dd449d433582bbb995d98673891fd64
L-TANK <214927409@qq.com>
2020-12-27 11:26:54 +0800

    单词规律 修改注释

--- LeetCode290.java
+++ LeetCode290.java
@@ -17,16 +17,18 @@ public class LeetCode290 {
         HashMap<String, Character> map = new HashMap<>();
         for (int i = 0; i < patterns.length; i++) {
             if(!map.containsKey(strs[i])){//若map中不含有对应的key
-                if(used[patterns[i] - 'a']){//但patterns标记了该key存在
+                if(used[patterns[i] - 'a']){//但used标记了该key存在
                     return false;//则出现冲突，返回错误
                     //例如 dog cat cat pig
                     //    a    b    b  a
                     //此时pig进入发现map无该key 但patterns为true
                 }
-                map.put(strs[i],patterns[i]);//将dog-a放入
+                map.put(strs[i],patterns[i]);//将dog-a cat-b放入
                 used[patterns[i] - 'a'] = true;
             }else {//若map中含有对应的key
-                if(map.get(strs[i]) != patterns[i]) return false;
+                if(map.get(strs[i]) != patterns[i]){
+                    return false;
+                }
                 //对当前key对应的value和该位置上的patterns进行比对 若冲突则报错
             }
         }
